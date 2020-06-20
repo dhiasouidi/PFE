005d954aa22fa382bdaa97d9d2849582ea1bf6b0
dhiasouidi <dhiasouidii@gmail.com>
2020-06-20 12:07:37 +0100

    demande de stage created

---
+++ app/Http/Controllers/DemandeDeStageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DemandeDeStage;
+use Illuminate\Http\Request;
+
+class DemandeDeStageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DemandeDeStage  $demandeDeStage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DemandeDeStage $demandeDeStage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DemandeDeStage  $demandeDeStage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DemandeDeStage $demandeDeStage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DemandeDeStage  $demandeDeStage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DemandeDeStage $demandeDeStage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DemandeDeStage  $demandeDeStage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DemandeDeStage $demandeDeStage)
+    {
+        //
+    }
+}

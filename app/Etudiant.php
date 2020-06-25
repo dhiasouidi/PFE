d976070dfcdbef1ad31c49a963acff03e7a27176
dhiasouidi <dhiasouidii@gmail.com>
2020-06-25 13:56:02 +0100

    binomes invited and invited_by

--- app/Etudiant.php
+++ app/Etudiant.php
@@ -22,12 +22,23 @@ class Etudiant extends Model
         return $this->hasMany('App\DemandeDeStage');
     }
 
+    function binomes_invited()
+    {
+        return $this->belongsTo('App\Binome', 'etudiant_id', 'binome_id')
+        ->withPivot('accepted'); // or to fetch accepted value
+    }
+
+    function binomes_invited_by()
+    {
+        return $this->belongsTo('App\Binome', 'binome_id', 'etudiant_id')
+        ->withPivot('accepted'); // or to fetch accepted value
+    }
+
     public function binome()
     {
         return $this->belongsTo('App\Binome', 'etudiant_id', 'binome_id')
                     ->wherePivot('accepted', '=', 1);
     }
-
 
 
 }

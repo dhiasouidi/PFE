1c30a6957fb5d2561f8b4b8fb5f086c04b11522e
dhiasouidi <dhiasouidii@gmail.com>
2020-06-26 15:15:14 +0100

    added sujets migration

--- database/migrations/2020_06_25_112908_create_sujets_table.php
+++ database/migrations/2020_06_25_112908_create_sujets_table.php
@@ -15,6 +15,23 @@ class CreateSujetsTable extends Migration
     {
         Schema::create('sujets', function (Blueprint $table) {
             $table->bigIncrements('ID_SUJET');
+
+            $table->string('TYPE_DEPOT');
+            $table->string('SESSION_ECRIT');
+            $table->string('SESSION_DEPOT');
+
+            $table->string('TITRE_SUJET');
+            $table->string('ABSTRACT');
+
+            $table->string('ENCADRANT');
+            $table->string('STATUT_ENCADRANT');
+
+            $table->string('STRUCTURE_RECHERCHE');
+
+            $table->date('DATE_DEPOT');
+
+            $table->foreign('ENCADRANT')->references('ID_ENSEIGNANT')->on('enseignants')->onDelete('cascade');
+
             $table->timestamps();
         });
     }

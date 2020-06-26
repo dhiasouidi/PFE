720ebb5538f53e732ebc4c8a1e1f4047bd8468a0
dhiasouidi <dhiasouidii@gmail.com>
2020-06-26 17:09:23 +0100

    add stage id to etudiants

---
+++ database/migrations/2020_06_26_160602_add_stage_id_to_etudiants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStageIdToEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('etudiants', function (Blueprint $table) {
+
+            $table->string('STAGE_ID')->nullable();
+
+            $table->foreign('STAGE_ID')->references('ID_STAGE')->on('stages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('etudiants', function (Blueprint $table) {
+            //
+        });
+    }
+}
